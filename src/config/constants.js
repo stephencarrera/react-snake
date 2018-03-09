c4adf836ece027df59cc042196ceb916e579ec27
Stephen Carrera <stephencarrera@users.noreply.github.com>
2018-03-09 14:15:44 -0800

    constants, styled components

---
+++ src/config/constants.js
@@ -0,0 +1,8 @@
+export const BOARD = 'BOARD';
+export const PLAYER = 'PLAYER';
+export const FOOD = 'FOOD';
+
+export const UP = 'UP';
+export const DOWN = 'DOWN';
+export const LEFT = 'LEFT';
+export const RIGHT = 'RIGHT';
